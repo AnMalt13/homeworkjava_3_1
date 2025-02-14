96b278a5f1157ab8395e56e6cd9387517fa832ae
AnMalt13 <fortunio@tutanota.com>
2025-02-14 23:28:20 +1000

    initical commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 123456;
+        int rub = 20;
+
+        int answer = price / rub;
+
+        System.out.println(answer);
+    }
+}
